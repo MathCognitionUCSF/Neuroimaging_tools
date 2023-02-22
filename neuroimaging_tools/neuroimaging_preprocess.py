762d678c1401d1f0733d0c7efa7ef33376a0612b
RianBogley <rianbogley@gmail.com>
2023-02-22 11:51:21 -0800

    Uploading more template files and dc_rpacs_packager script.

--- neuroimaging_tools/neuroimaging_preprocess.py
+++ neuroimaging_tools/neuroimaging_preprocess.py
@@ -1,16 +1,9 @@
-# Create pretty visualizations by Margo Kersey
+# Neuroimaging Pre-Processing Tools
 # %%
 # Import packages
-import requests
-import pandas as pd
-import numpy as np
-from io import StringIO
-import matplotlib.pyplot as plt
+
+
 # %%
-def horiz_bar_plot(task_names, task_scores, save=True, save_path=''):
-    # input a dataframe for one pidn
-    fig, ax = plt.subplots(figsize=(4,8))
-    ax.barh(task_names, task_scores)
-    ax.set_xlabel('Percentile')
+# Define functions
 
 
